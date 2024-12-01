1069be4e5f8dcfde78e823bab839d3a297955e02
StockiesLad <60066663+FluffyBumblebees@users.noreply.github.com>
2024-12-01 19:21:27 +1100

    general tweaks

---
+++ kubejs/server_scripts/tweaks/data/midgame/functional/custom_portals.js
@@ -0,0 +1,20 @@
+recipes((event, funcs) => {
+     comfuncs.iterate([
+          'customportals:white_portal_catalyst',
+          'customportals:light_gray_portal_catalyst',
+          'customportals:gray_portal_catalyst',
+          'customportals:black_portal_catalyst',
+          'customportals:brown_portal_catalyst',
+          'customportals:red_portal_catalyst',
+          'customportals:orange_portal_catalyst',
+          'customportals:yellow_portal_catalyst',
+          'customportals:lime_portal_catalyst',
+          'customportals:green_portal_catalyst',
+          'customportals:cyan_portal_catalyst',
+          'customportals:light_blue_portal_catalyst',
+          'customportals:blue_portal_catalyst',
+          'customportals:purple_portal_catalyst',
+          'customportals:magenta_portal_catalyst',
+          'customportals:pink_portal_catalyst'
+     ], item => event.replaceInput({input: 'minecraft:ender_eye', output: item}, 'minecraft:ender_eye', '#forge:ingots/enderium'))
+})
