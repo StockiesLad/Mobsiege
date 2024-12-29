75d836339437c040f8e0ac0415e32b779177978a
StockiesLad <60066663+FluffyBumblebees@users.noreply.github.com>
2024-12-29 20:03:46 +1100

    add ash bricking

--- src/main/java/com/stockieslad/mobsiege/Mobsiege.java
+++ src/main/java/com/stockieslad/mobsiege/Mobsiege.java
@@ -2,6 +2,14 @@ package com.stockieslad.mobsiege;
 
 import com.mojang.logging.LogUtils;
 import net.jmb19905.CarbonizeClient;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ConcretePowderBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -11,6 +19,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -18,6 +27,10 @@ import org.slf4j.Logger;
 public class Mobsiege {
     public static final String MODID = "mobsiege";
     public static final Logger LOGGER = LogUtils.getLogger();
+
+    //Only register things that KubeJS absolutely can't do. This is messy but it'll have to work.
+    public static final Block ASH_CLAY = registerBlockAndItem("ash_clay", new Block(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.FLUTE).strength(0.6f).sound(SoundType.GRAVEL)));
+    public static final Block PACKED_ASH = registerBlockAndItem("packed_ash", new ConcretePowderBlock(ASH_CLAY, BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.SNARE).strength(0.75f).sound(SoundType.SAND)));
 
     public Mobsiege(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
@@ -44,5 +57,16 @@ public class Mobsiege {
             LOGGER.info("HELLO FROM CLIENT SETUP");
             new CarbonizeClient().onInitializeClient();
         }
+    }
+
+    public static ResourceLocation of(String path) {
+        return new ResourceLocation(MODID, path);
+    }
+
+    private static Block registerBlockAndItem(String path, Block block) {
+        var id = of(path);
+        ForgeRegistries.BLOCKS.register(id, block);
+        ForgeRegistries.ITEMS.register(id, new BlockItem(block, new Item.Properties()));
+        return block;
     }
 }
