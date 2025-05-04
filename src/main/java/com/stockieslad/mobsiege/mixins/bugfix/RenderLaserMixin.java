86740ad09340c0b3447e9b08aecebdf588435c41
StockiesLad <60066663+FluffyBumblebees@users.noreply.github.com>
2025-05-04 16:25:46 +1000

    fix laser mixin

--- src/main/java/com/stockieslad/mobsiege/mixins/bugfix/RenderLaserMixin.java
+++ src/main/java/com/stockieslad/mobsiege/mixins/bugfix/RenderLaserMixin.java
@@ -1,23 +1,24 @@
 package com.stockieslad.mobsiege.mixins.bugfix;
 
 import buildcraft.silicon.client.render.RenderLaser;
+import buildcraft.silicon.tile.TileLaser;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderLaser.class)
-public class RenderLaserMixin {
-    @SuppressWarnings("DataFlowIssue")
-    @Inject(method = "render*", at = @At("HEAD"), cancellable = true, remap = false)
-    private void mobsiege$skipRenderIfAir(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay, CallbackInfo ci) {
-        BlockEntity blockEntity = (BlockEntity) (Object) this;
-        //if (blockEntity.getLevel().getBlockState(blockEntity.getBlockPos()).isAir()) {
-        if (blockEntity.getBlockState().isAir()) {
+public abstract class RenderLaserMixin {
+    @Inject(
+            method = "render(Lbuildcraft/silicon/tile/TileLaser;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V",
+            at = @At(value = "INVOKE", target = "Lbuildcraft/silicon/client/render/RenderLaser;isPlayerWearingGoggles()Z"),
+            remap = false,
+            cancellable = true
+    )
+    private void mobsiege$skipRenderIfAir(TileLaser tile, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource, int combinedLight, int combinedOverlay, CallbackInfo ci) {
+        if (tile.getBlockState().isAir())
             ci.cancel();
-        }
     }
 }
